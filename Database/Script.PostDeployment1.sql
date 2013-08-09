91ad81ed889e6c1da2630666e5b8a32dc33b8f9e
Rob Richardson <robrich@robrich.org>
2013-08-09 10:23:39 -0700

    A post-deployment script for lookup data

---
+++ Database/Script.PostDeployment1.sql
@@ -0,0 +1,38 @@
+﻿/*
+POST DEPLOYMENT SCRIPT
+*/
+-- NodeType data
+MERGE INTO NodeType AS Target 
+USING (VALUES 
+  (1, N'Root'), 
+  (2, N'Branch'), 
+  (3, N'Leaf')
+) 
+AS Source (NodeTypeId, NodeTypeName) 
+ON Target.NodeTypeId = Source.NodeTypeId 
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET NodeTypeName = Source.NodeTypeName 
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (NodeTypeId, NodeTypeName) 
+VALUES (NodeTypeId, NodeTypeName) 
+-- delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE;
+
+-- Ensure root Document exists
+IF (SELECT COUNT(*) FROM dbo.Document WHERE DocumentId = 1) = 0
+BEGIN
+	SET IDENTITY_INSERT dbo.Document ON
+	INSERT INTO dbo.Document (
+		DocumentId, NodeTypeId, ParentDocumentId, NodeIdPath, Depth, Slug,
+		Name, PageContent, HeadContent, ScriptContent,
+		CreatedDate, ModifiedDate, IsActive
+	) VALUES (
+		1, 1, 1, '/1', 0, '',
+		'CMS', '', NULL, NULL,
+		GETDATE(), GETDATE(), 1
+	)
+	SET IDENTITY_INSERT dbo.Document ON
+END;

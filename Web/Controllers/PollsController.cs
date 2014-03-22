2b04e195a9b4fa23d7dc467cbf62daffe39214bc
mdelgert <g@mm0n2004>
2014-03-22 13:26:43 -0700

    Added kendoui css & js

---
+++ Web/Controllers/PollsController.cs
@@ -0,0 +1,21 @@
+﻿namespace BetaSigmaPhi.Web.Controllers
+{
+    using System.Web.Mvc;
+    using BetaSigmaPhi.Web.Filters;
+
+    [RequireAdmin]
+    public class PollsController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Category()
+        {
+            return this.View();
+        }
+
+    }
+}

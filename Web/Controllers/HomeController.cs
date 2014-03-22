50bb732a92a3f213bc66f702eacfc7896592efe1
Rob Richardson <robrich@robrich.org>
2014-03-21 22:59:43 -0700

    Can't see home page until authenticated, and some twitter bootstrap styling on login page

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿namespace BetaSigmaPhi.Web.Controllers {
 	using System.Web.Mvc;
+	using BetaSigmaPhi.Web.Filters;
 
+	[RequireLogin]
 	public class HomeController : Controller {
 
 		public ActionResult Index() {

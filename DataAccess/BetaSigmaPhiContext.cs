66991aa76bbf3c44a2a51e8df8997f4151bf3229
Rob Richardson <robrich@robrich.org>
2014-03-22 13:18:59 -0700

    Add new tables to DataContext

--- DataAccess/BetaSigmaPhiContext.cs
+++ DataAccess/BetaSigmaPhiContext.cs
@@ -6,18 +6,24 @@
 	using BetaSigmaPhi.Entity;
 
 	public interface IBetaSigmaPhiContext : IDisposable {
+		IDbSet<Category> Categories { get; }
 		IDbSet<Document> Documents { get; }
 		IDbSet<ErrorLog> ErrorLogs { get; }
+		IDbSet<Poll> Polls { get; }
 		IDbSet<User> Users { get; }
+		IDbSet<Vote> Votes { get; }
 		IDbSet<TEntity> GetTable<TEntity>() where TEntity : IEntity;
 		void DefeatChangeTracking<TEntity>(TEntity Entity) where TEntity : IEntity;
 		int SaveChanges();
 	}
 
 	public class BetaSigmaPhiContext : DbContext, IBetaSigmaPhiContext {
+		public IDbSet<Category> Categories { get; set; }
 		public IDbSet<Document> Documents { get; set; }
 		public IDbSet<ErrorLog> ErrorLogs { get; set; }
+		public IDbSet<Poll> Polls { get; set; }
 		public IDbSet<User> Users { get; set; }
+		public IDbSet<Vote> Votes { get; set; }
 
 		public IDbSet<TEntity> GetTable<TEntity>() where TEntity : IEntity {
 			return this.Set<TEntity>();

6d28f34f96baf1de3a4d9b0de565e7bd8703b711
badripennati <badripennati@outlook.com>
2014-03-22 16:20:13 -0700

    AddedPollsController

---
+++ Web/Models/UserPollViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BetaSigmaPhi.Web.Models
+{
+    public class UserPollViewModel
+    {
+        public int PollId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ElectedUserId { get; set; }
+        
+        public DateTime CreateDate { get; set; }
+    }
+}

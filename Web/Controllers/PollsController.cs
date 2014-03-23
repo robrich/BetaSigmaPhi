b47c723a2c4ad34a9e48f536b4baccdf93436b61
badripennati <badripennati@outlook.com>
2014-03-22 18:38:43 -0700

    added skeleton methods and views for polls and frequencies

--- Web/Controllers/PollsController.cs
+++ Web/Controllers/PollsController.cs
@@ -10,14 +10,18 @@
     using BetaSigmaPhi.Service;
     using System.Linq;
 
-    [RequireAdmin]
+    [RequireLogin]
     public class PollsController : Controller
     {
         private readonly IPollRepository pollRepository;
         private readonly IUserRepository userRepository;
         private readonly IUserIdentityService userService;
+        private readonly ICategoryRepository categoryRepository;
+        private readonly IFrequencyRepository frequencyRepository;
+        private readonly IVoteRepository voteRepository;
 
-        public PollsController(IPollRepository PollRepository, IUserIdentityService UserIdentityService, IUserRepository UserRepository)
+        public PollsController(IPollRepository PollRepository, IUserIdentityService UserIdentityService, IUserRepository UserRepository,
+            ICategoryRepository CategoryRepository, IFrequencyRepository FrequencyRepository, IVoteRepository VoteRepository)
         {
             if (PollRepository == null)
             {
@@ -34,11 +38,30 @@
                 throw new ArgumentNullException("UserIdentityService");
             }
 
+            if (CategoryRepository == null)
+            {
+                throw new ArgumentNullException("CategoryRepository");
+            }
+
+            if (FrequencyRepository == null)
+            {
+                throw new ArgumentNullException("FrequencyRepository");
+            }
+
+            if (VoteRepository == null)
+            {
+                throw new ArgumentNullException("VoteRepository");
+            }
+
 			this.pollRepository = PollRepository;
             this.userService = UserIdentityService;
             this.userRepository = UserRepository;
+            this.categoryRepository = CategoryRepository;
+            this.frequencyRepository = FrequencyRepository;
+            this.voteRepository = VoteRepository;
 		}
 
+        [RequireAdmin]
         public ActionResult Index()
         {
             return this.View();
@@ -49,6 +72,7 @@
             return this.View();
         }
 
+        #region User Polls
         public ActionResult CurrentPolls()
         {
             PollsWithEligibleUsersViewModel polls = new PollsWithEligibleUsersViewModel();
@@ -71,8 +95,75 @@
 
         [HttpPost]
         public ActionResult SubmitPoll()
+        {            
+            return View();
+        }
+        #endregion User Polls
+
+        #region Frequency
+
+        [RequireAdmin]
+        public ActionResult Frequencies()
+        { 
+            return View(); 
+        }
+
+        [RequireAdmin]
+        public ActionResult EditFrequency()
+        { 
+            return View(); 
+        }
+
+        [RequireAdmin]
+        public ActionResult DeleteFrequency()
+        { 
+            return View(); 
+        }
+
+        [RequireAdmin]
+        public ActionResult AddFrequency()
+        { 
+            return View(); 
+        }
+
+        #endregion Frequency
+
+        #region Admin Polls
+
+        [RequireAdmin]
+        public ActionResult GetAllPolls()
+        {
+            List<Poll> allPolls = this.pollRepository.GetAll();
+            return View(allPolls); 
+        }
+
+        [RequireAdmin]
+        public ActionResult EditPoll(int PollId)
+        { 
+            return View(); 
+        }
+
+        [RequireAdmin]
+        [HttpPost]
+        public ActionResult DeletePoll(int PollId)
+        { 
+            return View(); 
+        }
+
+        [RequireAdmin]
+        public ActionResult AddPoll()
+        { 
+            return View(); 
+        }
+
+        [RequireAdmin]
+        [HttpPost]
+        [ActionName("AddPoll")]
+        public ActionResult AddPollPost()
         {
             return View();
         }
+
+        #endregion Admin Polls
     }
 }

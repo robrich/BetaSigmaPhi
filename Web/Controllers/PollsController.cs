c042a153dceeb4dfe10876bba21e842ab6c437a7
badripennati <badripennati@outlook.com>
2014-03-22 17:20:58 -0700

    added polls controller

--- Web/Controllers/PollsController.cs
+++ Web/Controllers/PollsController.cs
@@ -2,10 +2,42 @@
 {
     using System.Web.Mvc;
     using BetaSigmaPhi.Web.Filters;
+    using BetaSigmaPhi.Web.Models;
+    using BetaSigmaPhi.Repository;
+    using System;
+    using System.Collections.Generic;
+    using BetaSigmaPhi.Entity;
+    using BetaSigmaPhi.Service;
+    using System.Linq;
 
     [RequireAdmin]
     public class PollsController : Controller
     {
+        private readonly IPollRepository pollRepository;
+        private readonly IUserRepository userRepository;
+        private readonly IUserIdentityService userService;
+
+        public PollsController(IPollRepository PollRepository, IUserIdentityService UserIdentityService, IUserRepository UserRepository)
+        {
+            if (PollRepository == null)
+            {
+                throw new ArgumentNullException("PollRepository");
+			}
+
+            if (UserIdentityService == null)
+            {
+                throw new ArgumentNullException("UserIdentityService");
+            }
+
+            if (UserRepository == null)
+            {
+                throw new ArgumentNullException("UserIdentityService");
+            }
+
+			this.pollRepository = PollRepository;
+            this.userService = UserIdentityService;
+            this.userRepository = UserRepository;
+		}
 
         public ActionResult Index()
         {
@@ -19,6 +51,19 @@
 
         public ActionResult CurrentPolls()
         {
+            PollsWithEligibleUsersViewModel polls = new PollsWithEligibleUsersViewModel();
+            List<Poll> availablePolls = this.pollRepository.GetActivePolls();
+            
+            foreach (Poll p in availablePolls)
+            {
+                PollWithEligibleUsers pWithUsers = new PollWithEligibleUsers();
+                pWithUsers.userPoll = p;
+
+                User previousWinner = this.pollRepository.GetWinnerForPreviousPoll(p.PollId);
+                int? userId = this.userService.GetCurrentUserId();
+                List<User> eligibleUsers = this.userRepository.GetAll().Where(x => x.UserId != userId && x.UserId != previousWinner.UserId).ToList();
+                pWithUsers.EligibleUsers = eligibleUsers;
+            }
 
             return View();
         }
